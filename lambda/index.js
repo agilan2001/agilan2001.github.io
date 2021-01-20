3a35bcfba6c6d8307de555eb8fa56aaa1d9e7e82
agilan2001 <agilanvlr2001@gmail.com>
2021-01-21 00:07:53 +0530

    view cnt lambda

--- lambda/index.js
+++ lambda/index.js
@@ -13,9 +13,9 @@ var serviceAccount = {
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cbg0u%40agilan-pro.iam.gserviceaccount.com"
-  };
+};
 
-  serviceAccount["private_key"] = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+serviceAccount["private_key"] = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -24,37 +24,51 @@ admin.initializeApp({
 
 var db = admin.database();
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'agilanvlr2001@gmail.com',
-        pass: 'tlbehdowmlmiqsda'
-    }
-});
 
 
 exports.handler = async (event) => {
     var body = JSON.parse(event.body);
-    db.ref("messages").push(body);
 
-    var mailOptions = {
-        from: 'agilanvlr2001@gmail.com',
-        to: 'agilanvlr2001@gmail.com',
-        subject: 'Agilan Pro MESSAGE NOTIFICATION',
-        html: `${body.name || 'Someone'} has sent a Message.<br><br>
+    var ret_val;
+
+    if (body.job == "msg") {
+        db.ref("messages").push(body);
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'agilanvlr2001@gmail.com',
+                pass: 'tlbehdowmlmiqsda'
+            }
+        });
+
+
+        var mailOptions = {
+            from: 'agilanvlr2001@gmail.com',
+            to: 'agilanvlr2001@gmail.com',
+            subject: 'Agilan Pro MESSAGE NOTIFICATION',
+            html: `${body.name || 'Someone'} has sent a Message.<br><br>
         Phone: ${body.tel}<br>
         Mail: ${body.mail}<br>
         Message: ${body.msg}`
-    };
-    await transporter.sendMail(mailOptions);
+        };
+        await transporter.sendMail(mailOptions);
+        ret_val = "DONE";
+    }
+    else if (body.job == "view_cnt") {
+        await db.ref("view_cnt").transaction((data)=>{
+            ret_val = data+1;
+            return ret_val;
+        })
+    }
     return ({
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "POST"
         },
-        body: JSON.stringify("DONE"),
+        body: JSON.stringify(ret_val),
     })
 
 };
